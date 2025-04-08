45156908460d6218d9be6acfb4127ab24fdc7b7b
LaurenceYang1218 <laurence.yang1218@gmail.com>
2025-04-08 13:42:56 +0800

    add assert to main.py in hw1-f branch

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("This is the code for NTU Cloud Native Assignment2")
 num1, num2 = 3, 4
-print(f"The result of the sum is {num1 * num2}")
+total = num1 * num2
+print(f"The result of the sum is {total}")
+assert total == num1 + num2
